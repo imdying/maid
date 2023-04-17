a07aef3e709008c3af61aef6e3bce777b4763fdf
Blank <74569969+imdying@users.noreply.github.com>
2023-04-17 07:16:17 +0400

    Update build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -6,7 +6,27 @@ $BuildVersion   ??= '0.0.0';
 $BuildVersionId ??= 'Gold';
 
 # User variables
-$cwd = $pwd.Path;
+$Project = 'Maid.sln';
 
 # User code
-Write-Host "Hello World";
+dotnet test @(
+    "$Project"
+    '-s'
+    './.runsettings'
+);
+
+if ($LASTEXITCODE -ne 0) {
+    throw;
+}
+
+dotnet pack @(
+    "$Project"
+    "/p:Version=$BuildVersion"
+    '/p:Configuration=Release'
+);
+
+if ($LASTEXITCODE -ne 0) {
+    throw;
+}
+
+# dotnet nuget push
