93cd5e549c540f9004d1539c0184744819dcbf72
Blank <74569969+imdying@users.noreply.github.com>
2023-04-17 20:13:04 +0400

    Update Sample

---
+++ playground/Sample/Commands/Greet.cs
@@ -0,0 +1,20 @@
+﻿namespace Sample.Commands;
+
+public sealed class Greet : Command
+{
+    [Argument(
+        1, 
+        "user",
+        Arity = ArgumentArity.ExactlyOne
+    )]
+    public string? User
+    {
+        get;
+        set;
+    }
+
+    protected override void Execute(object? args)
+    {
+        Console.WriteLine("You said your greetings to {0}.", User);
+    }
+}
---
+++ playground/Sample/Commands/Universe.cs
@@ -0,0 +1,20 @@
+﻿namespace Sample.Commands;
+
+public sealed class Universe : Command
+{
+    public sealed class Reset : Command
+    {
+        protected override void Execute(object? args)
+        {
+            Console.WriteLine("The universe has been reset.");
+        }
+    }
+
+    public sealed class Create : Command
+    {
+        protected override void Execute(object? args)
+        {
+            Console.WriteLine("BANG");
+        }
+    }
+}
